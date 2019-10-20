f86d323f395271998885300b87bee80f46f04615
崔庆才丨静觅 <1016903103@qq.com>
2019-10-20 10:50:08 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,22 +1,27 @@
 import json
-from multiprocessing import Pool
 import requests
 from requests.exceptions import RequestException
 import re
+import time
+
 
 def get_one_page(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.text
         return None
     except RequestException:
         return None
 
+
 def parse_one_page(html):
     pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
-                         +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
-                         +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
     items = re.findall(pattern, html)
     for item in items:
         yield {
@@ -25,13 +30,14 @@ def parse_one_page(html):
             'title': item[2],
             'actor': item[3].strip()[3:],
             'time': item[4].strip()[5:],
-            'score': item[5]+item[6]
+            'score': item[5] + item[6]
         }
+
 
 def write_to_file(content):
     with open('result.txt', 'a', encoding='utf-8') as f:
         f.write(json.dumps(content, ensure_ascii=False) + '\n')
-        f.close()
+
 
 def main(offset):
     url = 'http://maoyan.com/board/4?offset=' + str(offset)
@@ -42,7 +48,6 @@ def main(offset):
 
 
 if __name__ == '__main__':
-    pool = Pool()
-    pool.map(main, [i*10 for i in range(10)])
-    pool.close()
-    pool.join()
+    for i in range(10):
+        main(offset=i * 10)
+        time.sleep(1)
